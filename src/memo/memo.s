593a02af0860c673d3a64e7f16b7b236d78a0bcc
Nathan Anderson <nathandanielanderson@gmail.com>
2025-08-10 16:51:37 -0500

    changed syscall from '16' to sol_log_

--- src/memo/memo.s
+++ src/memo/memo.s
@@ -1,10 +1,10 @@
-.equ NUM_ACCOUNTS, 0x00;
-.equ DATA_LEN, 0x08;
-.equ DATA, 0x10;
+.equ NUM_ACCOUNTS, 0x00
+.equ DATA_LEN, 0x08
+.equ DATA, 0x10
 .globl entrypoint
 entrypoint:
   ldxdw r0, [r1+NUM_ACCOUNTS]
-  ldxdw r2. [r1+DATA_LEN]
+  ldxdw r2, [r1+DATA_LEN]
   add64 r1, DATA
-  call 16
+  call sol_log_
   exit
